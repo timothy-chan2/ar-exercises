15d6be04f45e7a0473d46bf27eccd086540f4df0
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-04 05:15:53 +0000

    Add code to create 3 stores in the stores table

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+s1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+s2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+s3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts "There are #{Store.all.count} stores in the database"
