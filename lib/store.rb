bf4c6249cdcfefc931a623efc866cfdc031dd0b0
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2022-01-04 16:28:12 +0000

    Add validations to enforce the business rules

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :stores_must_carry_apparel
+
+  def stores_must_carry_apparel
+    if (mens_apparel == false && womens_apparel == false) || !(mens_apparel || womens_apparel)
+      errors.add(:mens_apparel, "or womens apparel: Store must carry at least one of those types of apparel")
+    end
+  end
 end
